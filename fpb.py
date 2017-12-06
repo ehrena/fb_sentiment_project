be59adf0d4733bb02fce2bfbc841b843b6988765
Andrew Ehrenberg <ehrena@Andrews-MacBook-Air-4.local>
2017-12-06 13:38:07 -0500

    updating final project files

--- fpb.py
+++ fpb.py
@@ -1,3 +1,7 @@
+
+#Do high-sentiment posts correlate to more likes?
+#Day of Week
+
 import sqlite3
 import requests
 import json
@@ -13,21 +17,25 @@ from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 import webbrowser
 import matplotlib
 import matplotlib.pyplot as plt
+import plotly.plotly as py
+import plotly.graph_objs as go
+import fb_info
 credentials, project = google.auth.default()
 client = language.LanguageServiceClient(credentials = credentials)
-
+#plotly_api_key = "iCXiURqN9lSc4tCqE3E2"
 ###########FACEBOOK##############
 APP_ID     = '444901019190968'
 APP_SECRET = '465b0244019fabad1becec4c13534e88'
 baseurl = 'https://graph.facebook.com/me/posts'
-myToken = 'EAACEdEose0cBAOJID5ULZBGThTGN8EcZCs9RPbDiNZBSyOoGupBreceKcNq6qx8IhKUpIdPG4LVJ5onivjvZBZAD9MbGot43lYXDWsIAO4s5O1FEkItt4TfuYD2k4pBcQvAZCsZC3DQfMpIeagCGQ5qKZCMmaNZChHOco0VOeWJ4j3LW0CKjN17WfxnMNPjFGj2kZD'
+myToken = fb_info.token
 myUsername = 'iceberg630'
 fullUrl = baseurl+'/'+myUsername
 facebook_session = False
 #################################
 CACHE_FNAME = "final_project_cache.json" #Cache file for Facebook data
 NLP_CACHE = "nlp_cache.json" #Cache file for Google NLP data
-
+conn = sqlite3.connect("final.sqlite")
+cur = conn.cursor()
 ##Caching patterns for both set up here##
 try:
     cache_file = open(CACHE_FNAME,'r')
@@ -45,7 +53,7 @@ try:
 except:
     NLP_CACHE_DICTION = {}
 
-def makeFacebookRequest(baseURL, params = {}):
+"""def makeFacebookRequest(baseURL, params = {}):
     global facebook_session
     if not facebook_session:
         authorization_base_url = 'https://www.facebook.com/dialog/oauth'
@@ -60,42 +68,9 @@ def makeFacebookRequest(baseURL, params = {}):
         redirect_response = input('Paste the full redirect URL here: ')
         facebook_session.fetch_token(token_url, client_secret=APP_SECRET, authorization_response=redirect_response.strip())
     return facebook_session.get(baseURL, params=params)
-myRequest = makeFacebookRequest(baseurl)
+myRequest = makeFacebookRequest(baseurl)"""
 
-def fbRequest(access_token, user):
-    if user in CACHE_DICTION:
-        return CACHE_DICTION[user]
-    else:
-        graph = facebook.GraphAPI(access_token)
-        profile = graph.get_object(id= user, fields=['id', 'created time', 'message'], limit=100)
-        posts = graph.get_connections(profile['id'], 'posts')
-        myPosts = []
-        i = 0
-        while True and i<1000:
-            try:
-                # Perform some action on each post in the collection we receive from Facebook.              
-                [myPosts.append(post) for post in posts['data']]
-                # Attempt to make a request to the next page of data, if it exists.
-                posts = requests.get(posts['paging']['next']).json()
-                i+=1
-            except KeyError:
-                # When there are no more pages (['paging']['next']), break from the loop and stop paginating
-                break
-        CACHE_DICTION[user] = myPosts
-        try:
-            dumped_json_cache = json.dumps(CACHE_DICTION)
-            fw = open(CACHE_FNAME,"w")
-            fw.write(dumped_json_cache)
-            fw.close()
-            return CACHE_DICTION[user]
-        except:
-            print("This is not a valid search term. Please try again.")
-            return None
-        return myPosts
-
-posts = fbRequest(myToken, 'me')
-print(len(posts))
-
+#This is an OAuth configuration, but running it takes a while so I commented it out.
 def lang_analysis(text):
     document = language.types.Document(
     content = text,
@@ -104,14 +79,40 @@ def lang_analysis(text):
     sentiment = sentiment.document_sentiment
     return sentiment.score, sentiment.magnitude
 
+graph = facebook.GraphAPI(myToken)
+user = 'me'
+if user in CACHE_DICTION:
+    pass
+else:
+    profile = graph.get_object(id= user, limit=100)
+    posts = graph.get_connections(profile['id'], 'posts')
+    myPosts = []
+    i = 0
+    while True:
+        try:
+            # Perform some action on each post in the collection we receive from Facebook.              
+            [myPosts.append(post) for post in posts['data']]
+            # Attempt to make a request to the next page of data, if it exists.
+            posts = requests.get(posts['paging']['next']).json()
+            i+=1
+        except KeyError:
+            # When there are no more pages (['paging']['next']), break from the loop and stop paginating
+            break
+    CACHE_DICTION[user] = myPosts
+    try:
+        dumped_json_cache = json.dumps(CACHE_DICTION)
+        fw = open(CACHE_FNAME,"w")
+        fw.write(dumped_json_cache)
+        fw.close()
+    except:
+        print("This is not a valid search term. Please try again.")
+
 sentDict = {}
-conn = sqlite3.connect("final.sqlite")
-cur = conn.cursor()
+
 cur.execute('DROP TABLE IF EXISTS Posts')
 cur.execute('''
 CREATE TABLE Posts (id INTEGER, message TEXT, created_time TIMESTAMP, sentiment_score DECIMAL, magnitude DECIMAL)''')
-#print(facebook_posts)
-for post in posts[:500]: 
+for post in CACHE_DICTION[user][:500]: #500 posts is close to the max that the lang_analysis function can handle at once, even once the Google NLP data is
     if "message" in post:
         if post["message"] in NLP_CACHE_DICTION:
             analysis = NLP_CACHE_DICTION[post["message"]]
@@ -133,6 +134,15 @@ for post in posts[:500]:
                 print("This is not a valid search term. Please try again.")
 
 #print((sentDict))
+post_ids = cur.execute("SELECT id FROM Posts")
+post_id_list = []
+for row in post_ids:
+    post_id_list.append(row[0])
+post_id_list = post_id_list[:50]
+posts_from_id = graph.get_objects(ids = post_id_list, fields = 'message, place, created_time, likes')
+print(posts_from_id)
+
+
 dates = []
 for row in cur.execute("SELECT created_time FROM Posts ORDER BY created_time"):
     f = '%Y-%m-%dT%H:%M:%S+0000'
@@ -146,15 +156,31 @@ magnitudes = [row[0] for row in cur.execute("SELECT magnitude FROM Posts ORDER B
 month_dict = {}
 for i in range(1, 13):
     for row in cur.execute("SELECT * FROM Posts ORDER BY created_time"):
-        if re.match("^[0-9]{4}-[0]+" + str(i) + "-[0-9]{2}.*", row[2]) and i not in month_dict:
+        if re.match("^[0-9]{4}-[0]?" + str(i) + "-[0-9]{2}.*", row[2]) and i not in month_dict:
             month_dict[i] = [row[3]]
-        elif re.match("^[0-9]{4}-[0]+" + str(i) + "-[0-9]{2}.*", row[2]) and i in month_dict:
+        elif re.match("^[0-9]{4}-[0]?" + str(i) + "-[0-9]{2}.*", row[2]) and i in month_dict:
             month_dict[i].append(row[3])
         else:
             pass
-print(month_dict)
-"""for i in range(1, 13):
-    print(sum(month_dict[i]))"""
+#print(month_dict)
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month_sentiments = {}
+for i in range(1, 13):
+    month_sentiments[months[i-1]] = sum(month_dict[i])/len(month_dict[i])
+#print(month_sentiments)
+myKeys = month_sentiments.keys()
+myValues = month_sentiments.values()
+month_bar = [go.Bar(
+                    x = list(myKeys) ,
+                    y = list(myValues)
+            )]
+layout = go.Layout(
+    title="Andrew's average sentiment on Facebook by month (since 2014)",
+)
+data = month_bar
+fig = go.Figure(data = data, layout = layout)
+py.iplot(fig, filename='basic-bar')
+
 
 conn.commit()
 conn.close()
